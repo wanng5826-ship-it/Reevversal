5d86b7626e1a1c5edde24109e76da2c23d82b15f
wanng5826-ship-it <wanng5826@gmail.com>
2026-06-01 20:33:28 +0700

    Update Telegram bot to use asyncio for execution
    Refactor Telegram bot to use asyncio for starting and running.

--- wan.py
+++ wan.py
@@ -1,16 +1,22 @@
 from telethon import TelegramClient, events
 from telethon.sessions import StringSession
 import os
+import asyncio
 
 api_id = 36038616
 api_hash = "7a0977625d58d2e0d81c4178f49bff31"
 session_string = os.environ.get("SESSION_STRING")
+BOT_USERNAME = "@JBAZ_bot"
 
 client = TelegramClient(StringSession(session_string), api_id, api_hash)
 
-@client.on(events.NewMessage(incoming=True, func=lambda e: e.is_private))
+@client.on(events.NewMessage(incoming=True, from_users=BOT_USERNAME))
 async def handler(event):
     await event.reply(event.text)
 
-client.start()
-client.run_until_disconnected()
+async def main():
+    await client.start()
+    print("Userbot jalan!")
+    await client.run_until_disconnected()
+
+asyncio.run(main())

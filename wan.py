506ad74ff43a00776af6c7995516d59db67e95bf
wanng5826-ship-it <wanng5826@gmail.com>
2026-06-01 19:48:03 +0700

    Refactor Telegram client initialization and session handling

--- wan.py
+++ wan.py
@@ -1,10 +1,13 @@
 from telethon import TelegramClient, events
+from telethon.sessions import StringSession
+import os
 
 api_id = 36038616
 api_hash = "7a0977625d58d2e0d81c4178f49bff31"
-BOT_USERNAME = "@JBAZ_bot"  # ganti dengan username Tradbot
+session_string = os.environ.get("SESSION_STRING")
+BOT_USERNAME = "username_tradbot"  # ganti username Tradbot
 
-client = TelegramClient('session', api_id, api_hash)
+client = TelegramClient(StringSession(session_string), api_id, api_hash)
 
 @client.on(events.NewMessage(incoming=True, from_users=BOT_USERNAME))
 async def handler(event):

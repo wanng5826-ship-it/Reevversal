6a2358b786e38c0f0b029de0c3506a31fde34428
wanng5826-ship-it <wanng5826@gmail.com>
2026-06-01 19:54:39 +0700

    Change bot username to @JBAZ_bot

--- wan.py
+++ wan.py
@@ -5,7 +5,7 @@ import os
 api_id = 36038616
 api_hash = "7a0977625d58d2e0d81c4178f49bff31"
 session_string = os.environ.get("SESSION_STRING")
-BOT_USERNAME = "username_tradbot"  # ganti username Tradbot
+BOT_USERNAME = "@JBAZ_bot" # ganti username Tradbot
 
 client = TelegramClient(StringSession(session_string), api_id, api_hash)
 

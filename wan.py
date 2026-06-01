47cb3746e8daf273c50b0df7186b12dace69f896
wanng5826-ship-it <wanng5826@gmail.com>
2026-06-01 20:08:53 +0700

    Update wan.py

--- wan.py
+++ wan.py
@@ -5,11 +5,10 @@ import os
 api_id = 36038616
 api_hash = "7a0977625d58d2e0d81c4178f49bff31"
 session_string = os.environ.get("SESSION_STRING")
-BOT_USERNAME = "@JBAZ_bot" # ganti username Tradbot
 
 client = TelegramClient(StringSession(session_string), api_id, api_hash)
 
-@client.on(events.NewMessage(incoming=True, from_users=BOT_USERNAME))
+@client.on(events.NewMessage(incoming=True, func=lambda e: e.is_private))
 async def handler(event):
     await event.reply(event.text)
 
